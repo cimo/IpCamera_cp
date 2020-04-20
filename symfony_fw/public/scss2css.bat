86673c66f075fa62e840e9b5388f53abeb3ec42d
CIMO <simone.dagostino@gmail.com>
2020-04-20 17:53:48 +0900

    Update

--- symfony_fw/public/scss2css.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Scss2css - Windows
-
-set source="%cd%"
-
-sass --watch %source%\scss:%source%\css --style compressed
-
-pause
--- symfony_fw/public/scss2css.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-clear
-
-echo Scss2css - Linux
-
-source="$(dirname ${BASH_SOURCE[0]})"
-
-sass --watch $source/scss:$source/css --style compressed
